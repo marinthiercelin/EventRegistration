b3516c1009ef607567cab97ae911e6b83db1bbb8
sgaspoz <sebastien.gaspoz@epfl.ch>
2017-01-17 18:08:32 -0500

    part 1 complete

--- src/ca/mcgill/ecse321/eventregistration/application/EventRegistration.java
+++ src/ca/mcgill/ecse321/eventregistration/application/EventRegistration.java
@@ -2,21 +2,21 @@ package ca.mcgill.ecse321.eventregistration.application;
 
 import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
 import ca.mcgill.ecse321.eventregistration.persistence.PersistenceXStream;
-import ca.mcgill.ecse321.eventregistration.view.ParticipantPage;
+import ca.mcgill.ecse321.eventregistration.view.EventRegistrationPage;
 
 public class EventRegistration {
 	private static String fileName = "output/eventregistration.xml";
 
 	public static void main(String[] args) {
-		// load model
-		final RegistrationManager rm = PersistenceXStream.initializeModelManager(fileName);
+	    final RegistrationManager rm = PersistenceXStream.initializeModelManager(fileName);
 
-		// start UI
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				new ParticipantPage(rm).setVisible(true);
-			}
-		});
+	    // start UI
+	    java.awt.EventQueue.invokeLater(new Runnable() {
+	        public void run() {
+	            new EventRegistrationPage(rm).setVisible(true);
+	        }
+	    });	
+
 	}
 
 }
