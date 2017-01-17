41ee34a09c08c08f4479f8b70035e740c0763947
marinthiercelin <marin.thiercelin@epfl.ch>
2017-01-17 15:53:26 -0500

    1.8

--- src/ca/mcgill/ecse321/eventregistration/controller/EventRegistrationController.java
+++ src/ca/mcgill/ecse321/eventregistration/controller/EventRegistrationController.java
@@ -24,15 +24,44 @@ public class EventRegistrationController {
 		rm.addParticipant(p);
 		PersistenceXStream.saveToXMLwithXStream(rm);
 	}
-	
+
 	public void createEvent(String name, Date date, Time startTime, Time endTime) throws InvalidInputException {
+
+		String error = "";
+		if (name == null || name.trim().length() == 0)
+			error = error + "Event name cannot be empty! ";
+		if (date == null)
+			error = error + "Event date cannot be empty! ";
+		if (startTime == null)
+			error = error + "Event start time cannot be empty! ";
+		if (endTime == null)
+			error = error + "Event end time cannot be empty! ";
+		if (endTime != null && startTime != null && endTime.getTime() < startTime.getTime())
+			error = error + "Event end time cannot be before event start time!";
+		error = error.trim();
+		if (error.length() > 0)
+			throw new InvalidInputException(error);
+
 		Event e = new Event(name, date, startTime, endTime);
-	    rm.addEvent(e);
-	    PersistenceXStream.saveToXMLwithXStream(rm);
+		rm.addEvent(e);
+		PersistenceXStream.saveToXMLwithXStream(rm);
 	}
-	
+
 	public void register(Participant participant, Event event) throws InvalidInputException
 	{
+		String error = "";
+		if (participant == null)
+			error = error + "Participant needs to be selected for registration! ";
+		else if (!rm.getParticipants().contains(participant))
+			error = error + "Participant does not exist! ";
+		if (event == null)
+			error = error + "Event needs to be selected for registration!";
+		else if (!rm.getEvents().contains(event))
+			error = error + "Event does not exist!";
+		error = error.trim();
+		if (error.length() > 0)
+			throw new InvalidInputException(error);
+
 		Registration r = new Registration(participant, event);
 		rm.addRegistration(r);
 		PersistenceXStream.saveToXMLwithXStream(rm);
