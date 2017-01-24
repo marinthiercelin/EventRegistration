01d2ddbe2f50cc108cb18e7fd500336030179465
marinthiercelin <marin.thiercelin@epfl.ch>
2017-01-24 15:29:28 -0500

    reorganize repo 2

---
+++ EventRegistrationAndroid/app/src/main/java/ca/mcgill/ecse321/eventregistrationandroid/MainActivity.java
@@ -0,0 +1,82 @@
+package ca.mcgill.ecse321.eventregistrationandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationController;
+import ca.mcgill.ecse321.eventregistration.controller.InvalidInputException;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+import ca.mcgill.ecse321.eventregistration.persistence.PersistenceXStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RegistrationManager rm = null;
+    private String fileName;
+    String error = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Initialize file name and XStream
+        fileName = getFilesDir().getAbsolutePath() + "/eventregistration.xml";
+        rm = PersistenceXStream.initializeModelManager(fileName);
+        refreshData();
+    }
+
+    private void refreshData() {
+        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
+        tv.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addParticipant(View v) {
+        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
+        EventRegistrationController pc = new EventRegistrationController(rm);
+        try {
+            pc.createParticipant(tv.getText().toString());
+        } catch (InvalidInputException e) {
+            error = e.getMessage();
+        }
+        refreshData();
+    }
+}
